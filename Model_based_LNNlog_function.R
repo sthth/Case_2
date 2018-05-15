4eb58e06f9e56aa93725bfedafa9b844859e3262
sthth <sthth@food.dtu.dk>
2018-05-15 11:37:02 +0200

    including comments

--- Model_based_LNNlog_function.R
+++ Model_based_LNNlog_function.R
@@ -10,6 +10,20 @@ library(psych) #necessary for describe function
 iters <- 1e+04
 set.seed(1)
 
+#intake_prob: consumption on individual day (either 1 or 0)
+#ID_freq: ID numbers for individuals included in the frequency model
+#log_intake: log transformed positive consumption amounts
+#covariable1_f: covariable (age) included in frequency model
+#covariable1_a: covariable (age) included in amounts model
+#cofactor1_f: cofactor (sex) included in frequency model
+#cofactor1_a: cofactor (sex) included in amounts model
+#weight_table_f: population weights and input parameters for frequency model
+#weight_table_a: population weights and input parameters for amounts model
+#iters: iterations
+#df_f: degrees of freedom for polynomial fitted in frequency model
+#df_a: degrees of freedom for polynomial fitted in amounts model
+
+
 
 ##Model
 usual_intake <-
@@ -18,14 +32,14 @@ usual_intake <-
     
     ##frequency model
     
-    if(is.null(covariable1_f) == T){
+    if(is.null(covariable1_f) == T){ #no covariates in frequency model 
       if(is.null(cofactor1_f) == T){
         
         nvar_f <- 0
         freq_model <- glmer(intake_prob ~ (1 | ID_freq), family = "binomial", nAGQ = 10)
         
       }
-      else{
+      else{ #only cofactor
         
         nvar_f <- 1
         freq_model <- glmer(intake_prob ~ cofactor1_f + (1 | ID_freq), family = "binomial", nAGQ = 10)
@@ -33,18 +47,18 @@ usual_intake <-
       }
     }
     
-    if(is.null(covariable1_f) == F){
+    if(is.null(covariable1_f) == F){ #both covariable and cofactor in frequency model
       if(is.null(cofactor1_f) == F){
         
         nvar_f <- 2
-        freq_pol_cov1 <- poly(covariable1_f,degree = df_f, raw = F)
+        freq_pol_cov1 <- poly(covariable1_f,degree = df_f, raw = F) #fit polynomial to covariable
         freq_model <- glmer(intake_prob ~ freq_pol_cov1 + cofactor1_f + (1 | ID_freq), family = "binomial", nAGQ = 10)
         
       }
-      else{
+      else{ #only covariable
         
         nvar_f <- 1
-        freq_pol_cov1 <- poly(covariable1_f,degree = df_f, raw = F)
+        freq_pol_cov1 <- poly(covariable1_f,degree = df_f, raw = F) #fit polynomial to covariable
         freq_model <- glmer(intake_prob ~ freq_pol_cov1 + (1 | ID_freq), family = "binomial", nAGQ = 10)
         
       }
@@ -53,14 +67,14 @@ usual_intake <-
     ##amounts model
     
     
-    if(is.null(covariable1_a) == T){
+    if(is.null(covariable1_a) == T){ #no covariates in amounts model 
       if(is.null(cofactor1_a) == T){
         
         nvar_a <- 0
         amounts_model <- lmer(log_intake ~ (1|ID_amount), REML = F)
         
       }
-      else{
+      else{ #only cofactor
         
         nvar_a <- 1
         amounts_model <- lmer(log_intake ~ cofactor1_a + (1|ID_amount), REML = F)
@@ -68,32 +82,32 @@ usual_intake <-
       }
     }
     
-    if(is.null(covariable1_a) == F){
+    if(is.null(covariable1_a) == F){ #both covariable and cofactor in amounts model
       if(is.null(cofactor1_a) == F){
         
         nvar_a <- 2
-        amounts_pol_cov1 <- poly(covariable1_a, degree = df_a, raw = F)
+        amounts_pol_cov1 <- poly(covariable1_a, degree = df_a, raw = F) #fit polynomial to covariable
         amounts_model <- lmer(log_intake ~ amounts_pol_cov1 + cofactor1_a + (1|ID_amount), REML = F)
       }
-      else{
+      else{ #only covariable
         
-        nvar_a <- 1
-        amounts_pol_cov1 <- poly(covariable1_a, degree = df_a, raw = F)
+        nvar_a <- 1 
+        amounts_pol_cov1 <- poly(covariable1_a, degree = df_a, raw = F) #fit polynomial to covariable
         amounts_model <- lmer(log_intake ~ amounts_pol_cov1 + (1|ID_amount), REML = F)
       }
     }
     
     frequency_coef <- numeric(df_f + nvar_f)
     for(i in 1:(df_f + nvar_f)){
-      frequency_coef[i] <- coef(summary(freq_model))[i, "Estimate"]
+      frequency_coef[i] <- coef(summary(freq_model))[i, "Estimate"] #vector with fixed effects estimates from frequency model
     }
     
-    dispersion <- as.numeric(sqrt(VarCorr(freq_model)$ID))    #logit
+    dispersion <- as.numeric(sqrt(VarCorr(freq_model)$ID))    #dispersion parameter on logit scale
     
     
     amount_coef <- numeric(df_a + nvar_a)
     for(i in 1:(df_a + nvar_a)){
-      amount_coef[i] <- coef(summary(amounts_model))[i, "Estimate"]
+      amount_coef[i] <- coef(summary(amounts_model))[i, "Estimate"] #vector with fixed effects estimates from amounts model
     }
     
     sigma_between <- as.numeric(VarCorr(amounts_model)$ID)            #log between person variance
@@ -101,30 +115,30 @@ usual_intake <-
     
     ##frequency model
     
-    if(is.null(covariable1_f) == T){ #neither covariable1 nor cofactor1
+    if(is.null(covariable1_f) == T){ #no covariates in frequency model 
       if(is.null(cofactor1_f) == T){
         
         intake <- numeric()
         total_freq <- numeric()
         mu <- frequency_coef[1]
-        f <- inv.logit(rnorm(iters, mu, dispersion))
+        f <- inv.logit(rnorm(iters, mu, dispersion)) #distribution of consumption frequencies
         total_freq <- f
         
       }
       
-      else{ #only cofactor1
+      else{ #only cofactor
         
         intake <- numeric()
         total_freq <- numeric()
         
-        for(i in 1:(nrow(weight_table_f)))
+        for(i in 1:(nrow(weight_table_f))) #for each "category" of individuals in the population weight table for the frequency model
         {
           mu <- frequency_coef[1]
           for(j in 1:(nvar_f))
           {
-            mu <- mu + frequency_coef[j+nvar_f] * weight_table_f[i,nvar_f]
+            mu <- mu + frequency_coef[j+nvar_f] * weight_table_f[i,nvar_f] #mean = mu + fixed effect estimate j+nvar_f * input covariate from weight table
           }
-          f <- inv.logit(rnorm(weight_table_f[i,"w"], mu, dispersion))
+          f <- inv.logit(rnorm(weight_table_f[i,"w"], mu, dispersion)) #distribution of consumption frequencies using population weights
           total_freq <- c(total_freq,f)
         }
       }
@@ -134,44 +148,44 @@ usual_intake <-
       if(is.null(cofactor1_f) == F){
         
         covariable1_f <- sort(covariable1_f, decreasing = F)
-        freq_pol <- poly(covariable1_f, df_f, raw =F)
+        freq_pol <- poly(covariable1_f, df_f, raw =F) #fit polynomial to covariable
         freq_pol <- cbind(covariable1_f, freq_pol)
         freq_pol <- aggregate(freq_pol, by = list(freq_pol[,1]), FUN = mean)
         freq_pol[,1] <- NULL
         names(freq_pol)[1] <- "covariable1"
-        weights_f <- merge(freq_pol, weight_table_f, by = "covariable1")
+        weights_f <- merge(freq_pol, weight_table_f, by = "covariable1") #make new population weight table adding polynomial estimates for covariable
         
         
         intake <- numeric()
         total_freq <- numeric()
-        for(i in 1:(nrow(weights_f))){
+        for(i in 1:(nrow(weights_f))){ #for each "category" of individuals in the population weight table for the frequency model
           mu <- frequency_coef[1]
           for(j in 1:df_f+1){
-            mu <- mu + frequency_coef[j+1] * weights_f[i,j+1]
+            mu <- mu + frequency_coef[j+1] * weights_f[i,j+1] #mean = mu + fixed effect estimate j+nvar_f * input covariate/polynomial estimate from weight table
           }
-          f <- inv.logit(rnorm(weights_f[i,"w"], mu, dispersion))
+          f <- inv.logit(rnorm(weights_f[i,"w"], mu, dispersion)) #distribution of consumption frequencies using population weights
           total_freq <- c(total_freq,f)
         }
       }
       else{ #only covariable1
         
         covariable1_f <- sort(covariable1_f, decreasing = F)
-        freq_pol <- poly(covariable1_f, df_f, raw =F)
+        freq_pol <- poly(covariable1_f, df_f, raw =F) #fit polynomial to covariable
         freq_pol <- cbind(covariable1_f, freq_pol)
         freq_pol <- aggregate(freq_pol, by = list(freq_pol[,1]), FUN = mean)
         freq_pol[,1] <- NULL
         names(freq_pol)[1] <- "covariable1"
-        weights_f <- merge(freq_pol, weight_table_f, by = "covariable1")
+        weights_f <- merge(freq_pol, weight_table_f, by = "covariable1") #make new population weight table adding polynomial estimates for covariable
         
         
         intake <- numeric()
         total_freq <- numeric()
-        for(i in 1:(nrow(weights_f))){
+        for(i in 1:(nrow(weights_f))){ #for each "category" of individuals in the population weight table for the frequency model
           mu <- frequency_coef[1]
           for(j in 1:nvar_f){
-            mu <- mu + frequency_coef[j+1] * weights_f[i,j+1]
+            mu <- mu + frequency_coef[j+1] * weights_f[i,j+1] #mean = mu + fixed effect estimate j+nvar_f * input covariate/polynomial estimate from weight table
           }
-          f <- inv.logit(rnorm(weights_f[i,"w"], mu, dispersion))
+          f <- inv.logit(rnorm(weights_f[i,"w"], mu, dispersion)) #distribution of consumption frequencies using population weights
           total_freq <- c(total_freq,f)
         }
       }
@@ -180,26 +194,26 @@ usual_intake <-
     
     ##amounts model
     
-    if(is.null(covariable1_a) == T){
+    if(is.null(covariable1_a) == T){ #no covariates
       if(is.null(cofactor1_a) == T){
         
         total_amount <- numeric()
         mu <- amount_coef[1]
-        a <- exp(rnorm(iters, mu, sqrt(sigma_between))+ sigma_within/2)
+        a <- exp(rnorm(iters, mu, sqrt(sigma_between))+ sigma_within/2) #distribution of back-transformed bias-corrected intake amounts
         total_amount <- a
         
       }
-      else{
+      else{ #only cofactor
         
         total_amount <- numeric()
-        for(i in 1:(nrow(weight_table_a))){
+        for(i in 1:(nrow(weight_table_a))){ #for each "category" of individuals in the population weight table for the amounts model
           
           mu <- amount_coef[1]
           for(j in 1:(nvar_a))
           {
-            mu <- mu + amount_coef[j+nvar_a] * weight_table_a[i, nvar_a]
+            mu <- mu + amount_coef[j+nvar_a] * weight_table_a[i, nvar_a] #mean = mu + fixed effect estimate j+nvar_f * input covariate from weight table
           }
-          a <- exp(rnorm(weight_table_a[i,"w"], mu, sqrt(sigma_between))+ sigma_within/2)
+          a <- exp(rnorm(weight_table_a[i,"w"], mu, sqrt(sigma_between))+ sigma_within/2) #distribution of back-transformed bias-corrected intake amounts, using population weights
           total_amount <- c(total_amount,a)
         }
       }
@@ -210,41 +224,41 @@ usual_intake <-
       if(is.null(cofactor1_a) == F){
         
         covariable1_a <- sort(covariable1_a, decreasing = F)
-        amount_pol <- poly(covariable1_a, df_a, raw = F)
+        amount_pol <- poly(covariable1_a, df_a, raw = F) #fit polynomial to covariable
         amount_pol <- cbind(covariable1_a, amount_pol)
         amount_pol <- aggregate(amount_pol, by = list(amount_pol[,1]), FUN = mean)
         amount_pol[,1] <- NULL
         names(amount_pol)[1] <- "covariable1"
-        weights_a <- merge(amount_pol, weight_table_a, by = c("covariable1"))
+        weights_a <- merge(amount_pol, weight_table_a, by = c("covariable1")) #make new population weight table adding polynomial estimates for covariable
         
         total_amount <- numeric()
-        for(i in 1:(nrow(weights_a))){
+        for(i in 1:(nrow(weights_a))){ #for each "category" of individuals in the population weight table for the amounts model
           mu <- amount_coef[1]
           for(j in 1:df_a){
-            mu <- mu + amount_coef[j+nvar_a] * weights_a[i, j+nvar_a]
+            mu <- mu + amount_coef[j+nvar_a] * weights_a[i, j+nvar_a] #mean = mu + fixed effect estimate j+nvar_f * input covariate/polynomial estimate from weight table
           }
-          a <- exp(rnorm(weights_a[i,"w"], mu, sqrt(sigma_between))+ sigma_within/2)
+          a <- exp(rnorm(weights_a[i,"w"], mu, sqrt(sigma_between))+ sigma_within/2) #distribution of back-transformed bias-corrected intake amounts, using population weights
           total_amount <- c(total_amount,a)
         }
       }
       
-      else{
+      else{ #only covariable
         
         covariable1_a <- sort(covariable1_a, decreasing = F)
-        amount_pol <- poly(covariable1_a, df_a, raw = F)
+        amount_pol <- poly(covariable1_a, df_a, raw = F) #fit polynomial to covariable
         amount_pol <- cbind(covariable1_a, amount_pol)
         amount_pol <- aggregate(amount_pol, by = list(amount_pol[,1]), FUN = mean)
         amount_pol[,1] <- NULL
         names(amount_pol)[1] <- "covariable1"
-        weights_a <- merge(amount_pol, weight_table_a, by = c("covariable1"))
+        weights_a <- merge(amount_pol, weight_table_a, by = c("covariable1")) #make new population weight table adding polynomial estimates for covariable
         
         total_amount <- numeric()
-        for(i in 1:(nrow(weights_a))){
+        for(i in 1:(nrow(weights_a))){ #for each "category" of individuals in the population weight table for the amounts model
           mu <- amount_coef[1]
           for(j in 1:df_a){
-            mu <- mu + amount_coef[j+nvar_a] * weights_a[i, j+nvar_a]
+            mu <- mu + amount_coef[j+nvar_a] * weights_a[i, j+nvar_a] #mean = mu + fixed effect estimate j+nvar_f * input covariate/polynomial estimate from weight table
           }
-          a <- exp(rnorm(weights_a[i,"w"], mu, sqrt(sigma_between))+ sigma_within/2)
+          a <- exp(rnorm(weights_a[i,"w"], mu, sqrt(sigma_between))+ sigma_within/2) #distribution of back-transformed bias-corrected intake amounts, using population weights
           total_amount <- c(total_amount,a)
           
         }
@@ -252,7 +266,7 @@ usual_intake <-
     }
     
     
-    intake <- total_freq * total_amount
+    intake <- total_freq * total_amount #combining intake frequencies and amounts
     intake <<- intake
     
     
